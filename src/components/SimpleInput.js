ad7fa042827519053caf1a0b715062ac58d86230
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-07-18 23:52:16 +0500

    added name classes for valid and invalid states

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   const enteredValue = useRef();
 
   const nameInputChangeHandler = (event) => {
@@ -10,23 +11,37 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
     console.log(enteredName);
     console.log(enteredValue.current.value);
 
-    setEnteredName('');
-    enteredValue.current.value = '';
+    setEnteredName("");
+    enteredValue.current.value = "";
   };
+
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={enteredValue}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
