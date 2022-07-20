093d25e92f5d5b54c32d80cf0b3f95d2733560c3
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-07-20 16:06:56 +0500

    added another input field (email); added its code for validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,40 +1,38 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
   const enteredValue = useRef();
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  const enteredEmailIsValid = enteredEmail.includes("@");
+  const emailInputInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
   let formIsValid = false;
 
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
 
-  // name is empty     => invalid => (false)
-  // name is not empty => valid   => (true)
-
-  // name is empty at the start so entered name flag is invalid  => false
-  // when name is entered, we set the entered name flag to valid => true
-
-  // name field is not touched at the start, so enteredNameTouched flag is (false)
-  // when name field is blurred, it means it got touched, so enteredNameTouched flag should be (true)
-
   const nameInputChangeHandler = (event) => {
-    // save name in state
     setEnteredName(event.target.value);
-
-    // if name entered is valid (not empty), set name is valid to true
   };
 
   const nameInputBlurHandler = (event) => {
-    // the input is blurred means it was touched, set name touched field to true
     setEnteredNameTouched(true);
+  };
 
-    // if entered name is empty (not valid), set name is valid to false
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
   };
 
   const formSubmissionHandler = (event) => {
@@ -42,7 +40,7 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
@@ -51,10 +49,17 @@ const SimpleInput = (props) => {
 
     setEnteredName("");
     setEnteredNameTouched(false);
+
+    setEnteredEmail("");
+    setEnteredEmailTouched(false);
     // enteredValue.current.value = ""; => NOT IDEAL! Not an ideal way to manipulate dom in react
   };
 
   const nameInputClasses = nameInputInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = emailInputInvalid
     ? "form-control invalid"
     : "form-control";
 
@@ -72,6 +77,19 @@ const SimpleInput = (props) => {
         />
         {nameInputInvalid && (
           <p className="error-text">Name must not be empty</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputInvalid && (
+          <p className="error-text">Email must not be empty</p>
         )}
       </div>
       <div className="form-actions">
